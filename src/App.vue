6096c3d5692c326d874b2fdc1c2c37ef350ce6fa
fabian <fabiannull@outlook.com>
2022-01-26 19:20:17 +0100

    prettier standard konfig, button , profilecard

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,5 @@
 <template>
   <div id="app">
-    <div class="lds-default" v-if="loadingState == true">
-      <div></div>
-      <div></div>
-      <div></div>
-      <div></div>
-      <div></div>
-      <div></div>
-      <div></div>
-      <div></div>
-      <div></div>
-      <div></div>
-      <div></div>
-      <div></div>
-    </div>
-    <button v-else v-on:click="loadUser">User anfordern</button>
-
-    <hr />
     <ProfileCard
       v-bind:userFirstName="userFirstName"
       v-bind:userLastName="userLastName"
@@ -24,16 +7,20 @@
       v-bind:userCity="userCity"
       v-bind:userCountry="userCountry"
       v-bind:userPicture="userPicture"
+      v-bind:userMailAdress="userMailAdress"
     ></ProfileCard>
+
+    <GenerateUserButton v-bind:loadingState="loadingState"></GenerateUserButton>
   </div>
 </template>
 
 <script>
-import ProfileCard from './components/ProfileCard.vue'
+import ProfileCard from './components/ProfileCard.vue';
+import GenerateUserButton from './components/GenerateUserButton.vue';
 
 export default {
   name: 'App',
-  components: { ProfileCard },
+  components: { ProfileCard, GenerateUserButton },
   data: () => ({
     loadingState: false,
     items: [],
@@ -42,120 +29,49 @@ export default {
     userAge: '',
     userCity: '',
     userCountry: '',
-    userPicture: ''
+    userPicture: '',
+    userMailAdress: '',
   }),
   methods: {
     async loadUser () {
-      this.loadingState = true
+      this.loadingState = true;
       //console.log('button geht')
-      let apiUrl = 'https://randomuser.me/api/'
+      let apiUrl = 'https://randomuser.me/api/';
 
       try {
-        let response = await this.axios.get(apiUrl)
-        this.items = response
-        this.userFirstName = response.data.results[0].name.first
-        this.userLastName = response.data.results[0].name.last
-        this.userAge = response.data.results[0].dob.age
-        this.userCity = response.data.results[0].location.city
-        this.userCountry = response.data.results[0].location.country
-        this.userPicture = response.data.results[0].picture.large
+        let response = await this.axios.get(apiUrl);
+        this.items = response;
+        this.userFirstName = response.data.results[0].name.first;
+        this.userLastName = response.data.results[0].name.last;
+        this.userAge = response.data.results[0].dob.age;
+        this.userCity = response.data.results[0].location.city;
+        this.userCountry = response.data.results[0].location.country;
+        this.userPicture = response.data.results[0].picture.large;
+        this.userMailAdress = response.data.results[0].email;
         //console.log(response)
       } catch (e) {
-        console.log(e)
+        console.log(e);
       }
-      this.loadingState = false
-    }
+      this.loadingState = false;
+    },
   },
   created () {
-    this.loadUser()
-  }
-}
+    this.loadUser();
+  },
+};
 </script>
 
 <style>
-.lds-default {
-  display: inline-block;
-  position: relative;
-  width: 80px;
-  height: 80px;
+@import url('https://fonts.googleapis.com/css2?family=Kumbh+Sans:wght@400;700&display=swap');
+
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+  font-family: 'Kumbh Sans', sans-serif;
 }
-.lds-default div {
-  position: absolute;
-  width: 6px;
-  height: 6px;
-  background: rgb(30, 27, 77);
-  border-radius: 50%;
-  animation: lds-default 1.2s linear infinite;
-}
-.lds-default div:nth-child(1) {
-  animation-delay: 0s;
-  top: 37px;
-  left: 66px;
-}
-.lds-default div:nth-child(2) {
-  animation-delay: -0.1s;
-  top: 22px;
-  left: 62px;
-}
-.lds-default div:nth-child(3) {
-  animation-delay: -0.2s;
-  top: 11px;
-  left: 52px;
-}
-.lds-default div:nth-child(4) {
-  animation-delay: -0.3s;
-  top: 7px;
-  left: 37px;
-}
-.lds-default div:nth-child(5) {
-  animation-delay: -0.4s;
-  top: 11px;
-  left: 22px;
-}
-.lds-default div:nth-child(6) {
-  animation-delay: -0.5s;
-  top: 22px;
-  left: 11px;
-}
-.lds-default div:nth-child(7) {
-  animation-delay: -0.6s;
-  top: 37px;
-  left: 7px;
-}
-.lds-default div:nth-child(8) {
-  animation-delay: -0.7s;
-  top: 52px;
-  left: 11px;
-}
-.lds-default div:nth-child(9) {
-  animation-delay: -0.8s;
-  top: 62px;
-  left: 22px;
-}
-.lds-default div:nth-child(10) {
-  animation-delay: -0.9s;
-  top: 66px;
-  left: 37px;
-}
-.lds-default div:nth-child(11) {
-  animation-delay: -1s;
-  top: 62px;
-  left: 52px;
-}
-.lds-default div:nth-child(12) {
-  animation-delay: -1.1s;
-  top: 52px;
-  left: 62px;
-}
-@keyframes lds-default {
-  0%,
-  20%,
-  80%,
-  100% {
-    transform: scale(1);
-  }
-  50% {
-    transform: scale(1.5);
-  }
+
+body {
+  background-color: rgb(24, 161, 175);
 }
 </style>
--- src/components/ProfileCard.vue
+++ src/components/ProfileCard.vue
@@ -1,6 +1,20 @@
 <template>
-  <div>
-    <img v-bind:src="userPicture" alt="Picture of Random User" />
+  <div class="profileCard">
+    <div>
+      <img class="upperPattern" src="../assets/bg-pattern-card.svg" alt="" />
+    </div>
+    <div class="content">
+      <img
+        class="profilePic"
+        v-bind:src="userPicture"
+        alt="Picture of Random User"
+      />
+      <h1>
+        {{ userFirstName }} {{ userLastName }} <span>{{ userAge }}</span>
+      </h1>
+      <h2>{{ userCity }} {{ userCountry }}</h2>
+      <h2 class="mailContent">{{ userMailAdress }}</h2>
+    </div>
   </div>
 </template>
 
@@ -16,9 +30,71 @@ export default {
     'userAge',
     'userCity',
     'userCountry',
-    'userPicture'
-  ]
-}
+    'userPicture',
+    'userMailAdress',
+  ],
+};
 </script>
 
-<style></style>
+<style scope>
+.profileCard {
+  max-width: 375px;
+  width: 90%;
+  margin: auto;
+  background-color: whitesmoke;
+  border-radius: 20px 20px 20px 20px;
+}
+
+.upperPattern {
+  width: 100%;
+  display: block;
+  border-radius: 20px 20px 0px 0px;
+}
+
+.profilePic {
+  border-radius: 50%;
+  border: 5px solid whitesmoke;
+  margin-top: -70px;
+  transition: transform 1s;
+}
+
+.profilePic:hover {
+  transform: scale(1.1);
+}
+
+.content {
+  text-align: center;
+}
+
+h1 {
+  font-weight: 700;
+  font-size: 1.7rem;
+  color: rgb(45, 50, 72);
+  margin-top: 1rem;
+}
+
+h1 span {
+  color: rgb(106, 111, 129);
+  font-weight: 400;
+  margin-left: 0.5rem;
+}
+
+h2 {
+  color: rgb(106, 111, 129);
+  margin-top: 0.6rem;
+  margin-bottom: 1.7rem;
+  font-weight: 400;
+  font-size: 1rem;
+}
+
+.mailContent {
+  padding-top: 1.5rem;
+  padding-bottom: 1.5rem;
+  border-top: rgb(106, 111, 129) 1px solid;
+}
+
+.content,
+.mailContent {
+  border-radius: 0px 0px 20px 20px;
+}
+</style>
